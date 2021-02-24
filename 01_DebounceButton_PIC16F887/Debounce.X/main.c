64f1a7540561fe5bfd63c015d36ce9533ab9f543
Mechatroniker94 <danny_elito94@outlook.com>
2021-02-23 20:08:29 -0600

    git ignore modificado

---
+++ 01_DebounceButton_PIC16F887/Debounce.X/main.c
@@ -0,0 +1,21 @@
+/*
+ * File:   main.c
+ * Author: danny
+ *
+ * Created on 23 de febrero de 2021, 07:58 PM
+ */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+
+
+void main(void) {
+    TRISD = 0xFF;
+    PORTD = 0x00;
+    while(1){
+        
+        
+    }//Ciclo infinito
+    return;
+}
